a82b0b25a0a8481b3b1dc322203a47429862e95b
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-21 15:25:00 -0700

    Brew formula update for trans version v0.1.4

--- trans.rb
+++ trans.rb
@@ -5,24 +5,24 @@
 class Trans < Formula
   desc ""
   homepage ""
-  version "0.1.3"
+  version "0.1.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "http://github.com/yulintan/trans/releases/v0.1.3/trans_0.1.3_Darwin_x86_64.tar.gz"
-    sha256 "98e3bc12998e63551f0cfd527130cff8d7d778a275cc6776c3c795130f0f6876"
+    url "http://github.com/yulintan/trans/releases/download/v0.1.4/trans_0.1.4_Darwin_x86_64.tar.gz"
+    sha256 "b73693fc967a260151192bfb2c414f613be5613c32ce2b0a715df7072418fde9"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "http://github.com/yulintan/trans/releases/v0.1.3/trans_0.1.3_Darwin_arm64.tar.gz"
-    sha256 "a7d33e0d9e0c8dfff2dafbfe091f5a7598a85b773d86ac97892e271f723663b8"
+    url "http://github.com/yulintan/trans/releases/download/v0.1.4/trans_0.1.4_Darwin_arm64.tar.gz"
+    sha256 "b711ab94f9c78060c02714be8436a137f8a231ffceef360ec9c8966a2f0beb3c"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "http://github.com/yulintan/trans/releases/v0.1.3/trans_0.1.3_Linux_x86_64.tar.gz"
-    sha256 "09155b24f7c7915e49c44e6c00264d6389cac6183f8ef716289bf6ac9a6f1267"
+    url "http://github.com/yulintan/trans/releases/download/v0.1.4/trans_0.1.4_Linux_x86_64.tar.gz"
+    sha256 "cb69f621ce399dc1fba45a66a359e774125424b1b471869028b533e8f541aebd"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "http://github.com/yulintan/trans/releases/v0.1.3/trans_0.1.3_Linux_arm64.tar.gz"
-    sha256 "3185a9261925b49097e5d1e677993924d3eac353261babd8fbce8318d0cd70ce"
+    url "http://github.com/yulintan/trans/releases/download/v0.1.4/trans_0.1.4_Linux_arm64.tar.gz"
+    sha256 "60cd7da2c1317b8c87fe7fd21d22d8b1f50db585ff03dbb8ec77ca62e510ceb2"
   end
 
   depends_on "git"
